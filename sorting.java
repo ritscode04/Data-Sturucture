ad8fe49eba8e5cd395a5ed7e91d8fbdbfecb765d
ritscode04 <86105724+ritscode04@users.noreply.github.com>
2022-03-16 15:22:13 +0530

    Sorting the element in arraylist
    This code helps you to sort the elements in ArrayList.

---
+++ sorting.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class sorting {
+       public static void main(String[] args){
+
+        ArrayList<Integer> sorting = new ArrayList<>();
+        //--------add element-----------
+       sorting.add(5);
+       sorting.add(10);
+       sorting.add(2);
+       System.out.println(sorting);
+
+       //----------sorting element---------
+     Collections.sort(sorting);
+     System.out.println("\n sorting element: " + sorting);
+       
+
+   
+
+
+    }
+} 
+
